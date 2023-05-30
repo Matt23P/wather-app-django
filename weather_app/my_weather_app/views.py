2331a88f1eafc3582893e3410168856e9175d53f
Mateusz Przybyl <236630@edu.p.lodz.pl>
2023-05-30 20:10:18 +0200

    issue with api key fixed

--- weather_app/my_weather_app/views.py
+++ weather_app/my_weather_app/views.py
@@ -5,7 +5,8 @@ import datetime
 def index(request):
     API_KEY = open("C:\\_studia\\wather-app-django\\weather_app\\my_weather_app\\api_key.txt", "r").read()
     current_weather_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
-    forecast_weather_url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}"
+    # forecast_weather_url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}"
+    forecast_weather_url = "https://api.openweathermap.org/data/2.5/forecast?lat={}&lon={}&appid={}"
 
     if request.method == "POST": 
         city1 = request.POST['city1']
@@ -35,21 +36,26 @@ def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url)
     print(api_key)
     forecast_response = requests.get(forecast_url.format(lat, lon, api_key)).json()
 
+    # print(forecast_response)
+
     weather_data = {
         "city": city,
+        "country": response['sys']['country'],
         "temperature": round(response['main']['temp'] - 273.15, 2),
         "description": response['weather'][0]['description'],
         "icon": response['weather'][0]['icon']
     }
 
     daily_forecasts = []
-    for daily_data in forecast_response['daily'][:5]:
+    for daily_data in forecast_response['list'][:5]: #['list'] 
         daily_forecasts.append({
             "day": datetime.datetime.fromtimestamp(daily_data['dt']).strftime("%A"),
-            "min_temp": round(daily_data['temp']['min'] - 273.15, 2),
-            "max_temp": round(daily_data['temp']['max'] - 273.15, 2),
+            "min_temp": round(daily_data['main']['temp_min'] - 273.15, 2),
+            "max_temp": round(daily_data['main']['temp_max'] - 273.15, 2),
             "description": daily_data['weather'][0]['description'],
             "icon": daily_data['weather'][0]['icon']
         })
     
+    print(daily_forecasts)
+
     return weather_data, daily_forecasts
